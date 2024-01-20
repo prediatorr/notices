2142f8e99f4fa481991727fab601fb140667be3d
prediatorr <rajeevkumarsingh5252@gmail.com>
2024-01-20 12:34:46 +0530

    added mongoose

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
+require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
-const routes = require("./routes/index")
-app.use("/user", routes.userRouter)
+const routes = require("./routes/index");
+app.use("/user", routes.userRouter);
 
 app.get("/", (req, res) => {
   res.send("Hi babes");
 });
 
-app.listen(3000, () => {
-  console.log("server is working");
-});
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASSWORD}@cluster0.4wnjk23.mongodb.net/?retryWrites=true&w=majority`,
+    {
+      dbName: process.env.DBNAME,
+    }
+  )
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("server is working");
+      console.log(process.env.DBNAME);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
